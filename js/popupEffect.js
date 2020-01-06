c3af94b0572f3bd9b74653834ad5ad5949966e69
LuuHuy040501 <57887752+LuuHuy040501@users.noreply.github.com>
2020-01-06 19:24:44 +0700

    add up to top bottom to homepage and product page(huy)

--- js/popupEffect.js
+++ js/popupEffect.js
@@ -1,7 +1,7 @@
 function toggle(n) {
     var productContainer = document.getElementById('product-container');
     productContainer.classList.toggle('active');
-    /* take the list of popup */
+    
     var popup = document.getElementById('popup-'+n);
     popup.classList.toggle("active");
 };
